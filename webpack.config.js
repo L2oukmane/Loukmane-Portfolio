ec35dc2753e523ed2734898e5da5acb6e4fe1172
Lucas Shangrita <oustanilokman@gmail.com>
2022-04-25 00:20:48 +0300

    Set up development mode for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-    title: 'Output Management',
+        title: 'Development',
     }),
   ],
   output: {
